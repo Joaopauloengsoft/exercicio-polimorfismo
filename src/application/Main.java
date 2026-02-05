596969f81a99cdf4948b7eb96bff13fd4c0e3f94
João Paulo Botelho <159064484+Joaopauloengsoft@users.noreply.github.com>
2026-02-05 14:44:10 -0300

    Polimorfismo

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,13 +1,52 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+package application;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+import entities.ImportedProduct;
+import entities.Product;
+import entities.UsedProduct;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Product> list = new ArrayList<>();
+
+        System.out.println("Enter the number of products: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Enter the product #" + i + ": ");
+            System.out.println("Common, used or imported (c/u/i)");
+            char ch = sc.next().charAt(0);
+            System.out.print("Name: ");
+            String name = sc.next();
+            System.out.print("Price: ");
+            double price = sc.nextDouble();
+            if (ch == 'c' || ch == 'C'){
+                list.add(new Product(name, price));
+            }
+            if (ch == 'u' || ch == 'U'){
+                System.out.print("Manufacture date (dd/MM/yyyy): ");
+                LocalDate date = LocalDate.parse(sc.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+                list.add(new UsedProduct(name, price, date));
+            }
+            if  (ch == 'i' || ch == 'I'){
+                System.out.println("Customs fee: ");
+                double customsFee = sc.nextDouble();
+                list.add(new ImportedProduct(name, price, customsFee));
+            }
+
+        }
+        System.out.println("---PRICE TAGS---");
+        for (Product p : list) {
+            System.out.println(p.priceTag());
+        }
+        sc.close();
+    }
 }
--- src/entities/ImportedProduct.java
+++ src/entities/ImportedProduct.java
@@ -1,4 +1,30 @@
 package entities;
 
-public class ImportedProduct {
+public class ImportedProduct extends Product{
+    private double customsFee;
+
+    public ImportedProduct() {
+        super();
+    }
+
+    public ImportedProduct(String name, double price, double customsFee) {
+        super(name, price);
+        this.customsFee = customsFee;
+    }
+
+    public double getCustomsFee() {
+        return customsFee;
+    }
+
+    public void setCustomsFee(double customsFee) {
+        this.customsFee = customsFee;
+    }
+
+    public String priceTag(){
+        return getName() + " || $ " + totalPrice() + " || Customns fee: $ " + getCustomsFee();
+    }
+
+    public Double totalPrice(){
+        return getPrice() + getCustomsFee();
+    }
 }
